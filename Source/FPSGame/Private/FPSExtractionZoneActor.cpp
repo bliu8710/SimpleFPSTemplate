9dfdaf93e406f67f13567264a650f867bff1b023
zhenbin.liu <zhenbin.liu@mihoyo.com>
2020-11-11 19:57:09 +0800

    【出口】进入出口打ue_log

---
+++ Source/FPSGame/Private/FPSExtractionZoneActor.cpp
@@ -0,0 +1,38 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "FPSExtractionZoneActor.h"
+#include "Components/BoxComponent.h"
+
+// Sets default values
+AFPSExtractionZoneActor::AFPSExtractionZoneActor()
+{
+	OverlapComp = CreateDefaultSubobject<UBoxComponent>(TEXT("OverlapComp"));
+	OverlapComp->SetCollisionEnabled(ECollisionEnabled::QueryOnly);
+	OverlapComp->SetCollisionResponseToAllChannels(ECR_Ignore);
+	OverlapComp->SetCollisionResponseToChannel(ECC_Pawn, ECR_Overlap);
+	OverlapComp->SetBoxExtent(FVector(200.0f));
+	RootComponent = OverlapComp;
+
+	/* 设置在editor可见 */
+	OverlapComp->SetHiddenInGame(false);
+
+	OverlapComp->OnComponentBeginOverlap.AddDynamic(this, &AFPSExtractionZoneActor::HandleOverlap);
+
+}
+
+
+
+// Called when the game starts or when spawned
+void AFPSExtractionZoneActor::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+void AFPSExtractionZoneActor::HandleOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp,
+	int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+	UE_LOG(LogTemp, Log, TEXT("Overlap Extract Zone"));
+}
+
